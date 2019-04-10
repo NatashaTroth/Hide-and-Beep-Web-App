8ee78a60c5e953ea9dc01ba3a8b81b5a9e454e52
Natasha Troth <natashatroth@c78-104-170-39.fh-salzburg.ac.at>
2019-04-10 15:30:09 +0200

    change forgot eail authentication type

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: "plain",
+    authentication: "Login",
     enable_starttls_auto: true,
     user_name: "hideAndBeep@gmail.com",
     password: "HidingAndBeeping5"
