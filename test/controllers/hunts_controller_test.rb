6a0cad2a10ceeb75c750b9b8eab2a98bb1ddd498
Natasha Troth <natashatroth@c78-104-170-39.fh-salzburg.ac.at>
2019-04-10 17:52:14 +0200

    add tests for logged in users

--- test/controllers/hunts_controller_test.rb
+++ test/controllers/hunts_controller_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 
+#user not logged in
 class HuntsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @hunt = hunts(:one)
@@ -48,3 +49,62 @@ class HuntsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to new_user_session_path
   end
 end
+
+
+#user logged in
+class HuntsControllerUserTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+
+
+  setup do
+
+    @hunt = hunts(:one)
+    @user = users(:one)
+    #post user_session_path, params: { email: 'test@test.at', encrypted_password: 'aSecret' }
+    # sign_in users(:one)
+    # get :index
+    # assert_response :success
+
+    sign_in(@user)
+
+  end
+
+  # def sign_in(user)
+  #   post user_session_path \
+  #     "user[email]"    => user.email,
+  #     "user[encrypted_password]" => user.encrypted_password
+  # end
+
+
+  test "should get index" do
+    get hunts_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_hunt_url
+    assert_response :success
+  end
+
+  # #WORKS - BUT NEED TO GET ID
+  test "should create hunt" do
+    assert_difference('Hunt.count') do
+      post hunts_url, params: { hunt: { expiry_date: @hunt.expiry_date, name: "newNameToAvoidUniqueConstraint", no_time_limit: @hunt.no_time_limit, set_time_limit: @hunt.set_time_limit, start_date: @hunt.start_date, winning_code: @hunt.winning_code } }
+    end
+   end
+
+
+
+end
+
+# class HuntsControllerUserTest < ActionController::TestCase
+#   include Devise::Test::ControllerHelpers
+
+#   test 'authenticated users can GET index' do
+#     sign_in users(:one)
+
+#     get :index
+#     assert_response :success
+#   end
+# end
