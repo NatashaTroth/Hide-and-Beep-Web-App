2976dc58041181ea182d473057b89078f93920bb
Natasha Troth <natashatroth@c78-104-170-101.fh-salzburg.ac.at>
2019-04-11 16:07:18 +0200

    Fix toggleSetTimeLimit

--- app/assets/javascripts/hunts.js
+++ app/assets/javascripts/hunts.js
@@ -1,27 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-// window.onload = function(){
-//     let noTimeLimitCheckbox = document.querySelector(".js-hunt-form-no-time-limit-checkbox")
-//     let setTimeLimitInputDiv = document.querySelector(".js-hunt-form-set-time-limit")
 
-//     console.log("Arrived " + noTimeLimitCheckbox)
-//     if(noTimeLimitCheckbox != null){
-//         //for first load and reload
-//         noTimeLimitCheckbox.addEventListener("click", toggleSetTimeLimit)
-//         toggleSetTimeLimit()
-//     }
+document.addEventListener('turbolinks:load', function(){
+    let noTimeLimitCheckbox = document.querySelector(".js-hunt-form-no-time-limit-checkbox")
+    let setTimeLimitInputDiv = document.querySelector(".js-hunt-form-set-time-limit")
+
+    if(noTimeLimitCheckbox != null){
+        //for first load and reload
+        toggleSetTimeLimit()
+        
+        noTimeLimitCheckbox.addEventListener("click", toggleSetTimeLimit)
+    }
 
 
-//     function toggleSetTimeLimit(){
-//         console.log("HERE")
-//         if(noTimeLimitCheckbox.checked){
-//             setTimeLimitInputDiv.classList.add("visibility-hidden")
-//             setTimeLimitInputDiv.classList.remove("visibility-visible")
+    function toggleSetTimeLimit(){
+        if(noTimeLimitCheckbox.checked){
+            setTimeLimitInputDiv.classList.add("visibility-hidden")
+            setTimeLimitInputDiv.classList.remove("visibility-visible")
 
-//         }
-//         else{
-//             setTimeLimitInputDiv.classList.add("visibility-visible")
-//             setTimeLimitInputDiv.classList.remove("visibility-hidden")
-//         }
-//     }    
-// };
+        }
+        else{
+            setTimeLimitInputDiv.classList.add("visibility-visible")
+            setTimeLimitInputDiv.classList.remove("visibility-hidden")
+        }
+    }    
+});
