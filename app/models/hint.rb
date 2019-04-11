c2f3c8b341879ec76d20c3aab083d99c54c58520
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2019-04-11 23:34:28 +0200

    fix styling in hint form with drag and drop position

--- app/models/hint.rb
+++ app/models/hint.rb
@@ -1,7 +1,7 @@
 class Hint < ApplicationRecord
     belongs_to :hunt
     validates :order, :location_name, :longitude, :latitude, :text, :hunt_id, presence: true
-    validates :order, :numericality => { :greater_than_or_equal_to => 0 }
+    #validates :order, :numericality => { :greater_than_or_equal_to => 0 }
     validates :longitude, :latitude, numericality: true 
     validates :text, length: { maximum: 200 }
 end
