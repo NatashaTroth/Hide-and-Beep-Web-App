cb55297e765520913c2e70837f6fdb4d0d95da65
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2019-04-12 11:31:20 +0200

    fix js request

--- app/assets/javascripts/hints.js
+++ app/assets/javascripts/hints.js
@@ -13,39 +13,41 @@ document.addEventListener("turbolinks:load", function() {
     });
 
 
-    let lat = document.getElementById('hint_latitude').value
-    let lng = document.getElementById('hint_longitude').value
+    let lat = document.getElementById('hint_latitude')
+    let lng = document.getElementById('hint_longitude')
+
+    if (lat != null && lng != null) {
+      lat = lat.value;
+      lng = lng.value;
+
+      if (lat.length == 0 && lng.length == 0) {
+        // set default value if user want to add a new hint -> default value: position of FH Salzburg
+        lat = 47.723730
+        lng = 13.086596
+      } 
   
-    if (lat.length == 0 && lng.length == 0) {
-      // set default value if user want to add a new hint -> default value: position of FH Salzburg
-      lat = 47.723730
-      lng = 13.086596
-    }
-  
-    var map = L.map('map').setView([lat, lng], 13);
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-    // set edit hint location in
-    L.marker([lat, lng]).addTo(map)
-      .bindPopup(`Current hint: ${lat}, ${lng}`)
-      .openPopup();
-  
-    var popup = L.popup();
+      var map = L.map('map').setView([lat, lng], 13);
+    
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    
+      // set edit hint location in
+      L.marker([lat, lng]).addTo(map)
+        .bindPopup(`Current hint: ${lat}, ${lng}`)
+        .openPopup();
+    
+      var popup = L.popup();
   
     function onMapClick(e) {
-      var lat = ""
-      var lng = ""
-  
-          popup
-              .setLatLng(e.latlng)
-              .setContent("You clicked the map at " + e.latlng.toString())
+      popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
   
       document.getElementById('hint_latitude').value = e.latlng.lat
       document.getElementById('hint_longitude').value = e.latlng.lng
     }
     map.on('click', onMapClick)
+    }
 });
