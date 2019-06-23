3dc60e44a1849c980c1f51513803caa46178beec
Natasha Troth <natashatroth@Natashas-MacBook-Pro.local>
2019-06-23 19:12:33 +0200

    Skip authentication if going to /privacy-policy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, :redirect_unauthorized_users, :only => [:privacy_policy]
   def home; end
 
   def help; end
 
   def privacy_policy; end
+
 end
